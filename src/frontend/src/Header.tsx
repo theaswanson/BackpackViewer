0b5c288ca449124540fb8d9aa4a769c5359e3754
Adam Swanson <adam.swanson.98@gmail.com>
2025-04-19 22:48:33 -0400

    wip: add custom dropdown

--- src/frontend/src/Header.tsx
+++ src/frontend/src/Header.tsx
@@ -1,8 +1,39 @@
+import { Children, PropsWithChildren, ReactNode, useState } from "react";
 import "./Header.css";
 
+const SelectOption = ({ children }: { children: ReactNode }) => (
+  <div className='select-option'>{children}</div>
+);
+
+const ItemBordersSelect = ({ children }: PropsWithChildren) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const selectedItem = Children.toArray(children)[0];
+
+  return (
+    <>
+      <div className='select' onClick={() => setIsOpen((open) => !open)}>
+        {selectedItem}
+      </div>
+
+      {isOpen ? <div>{children}</div> : undefined}
+    </>
+  );
+};
+
+const FilterOptions = () => (
+  <ItemBordersSelect>
+    <SelectOption>No Item Borders</SelectOption>
+    <SelectOption>Show quality color borders</SelectOption>
+    <SelectOption>Show marketable borders only</SelectOption>
+  </ItemBordersSelect>
+);
+
 export const Header = () => (
-  <div className='header'>
-    <span>{">>"}</span>
-    <h1>BACKPACK</h1>
+  <div className='header-wrapper'>
+    <div className='header'>
+      <span>{">>"}</span>
+      <h1>BACKPACK</h1>
+    </div>
+    <FilterOptions />
   </div>
 );
