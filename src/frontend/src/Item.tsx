649d60abbf69c78530bf5b320f5a214e41c09711
Adam Swanson <adam.swanson.98@gmail.com>
2025-04-19 21:03:40 -0400

    Update colors and item square size

--- src/frontend/src/Item.tsx
+++ src/frontend/src/Item.tsx
@@ -1,22 +1,20 @@
-import { ItemModel } from './models/ItemModel';
-import './Item.css'
-import clsx from 'clsx';
+import clsx from "clsx";
+import "./Item.css";
+import { ItemModel } from "./models/ItemModel";
 
 interface ItemProps {
-  item: ItemModel
+  item: ItemModel;
 }
 
 function Item(props: ItemProps) {
   return (
-    <div className={clsx("Item", {
-      untradable: !props.item.tradable
-    })}>
-      <img
-        src={props.item.url}
-        width={150}
-        height={150}
-        alt="Item icon"></img>
-      {props.item.quantity > 1 && <p>x{props.item.quantity}</p>}
+    <div
+      className={clsx("Item", {
+        untradable: !props.item.tradable,
+      })}
+    >
+      <img src={props.item.url} alt='Item icon'></img>
+      {/* {props.item.quantity > 1 && <p>x{props.item.quantity}</p>} */}
     </div>
   );
 }
