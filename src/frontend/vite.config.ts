7f34d8cb1963a6478104d4290bb5f30e0e57f8ca
Adam Swanson <adam.swanson.98@gmail.com>
2025-05-10 13:27:14 -0400

    try: configure deployment for frontend

--- src/frontend/vite.config.ts
+++ src/frontend/vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig((configEnv) => ({
+  base: configEnv.mode === "development" ? "/" : "/BackpackViewer/",
   plugins: [react()],
-})
+}));
